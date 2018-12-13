076f2206827606b55bec3f94107db22fca7ccbb8
Rey Matsunaga <rmatsun1@Reys-Mac-mini.local>
2018-12-13 14:06:28 -0800

    first commit

---
+++ ProgrammaticAutoLayout Practice/ProgrammaticAutoLayout Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgrammaticAutoLayout Practice
+//
+//  Created by Rey Matsunaga on 12/13/18.
+//  Copyright © 2018 Rey Matsunaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
