8fa670cfa765892c428d744008b5ceb958b950ea
Rey Matsunaga <rmatsun1@Reys-Mac-mini.local>
2018-12-13 16:45:44 -0800

    red view layout made programmatically

--- ProgrammaticAutoLayout Practice/ProgrammaticAutoLayout Practice/ViewController.swift
+++ ProgrammaticAutoLayout Practice/ProgrammaticAutoLayout Practice/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var redView : UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        redView.backgroundColor = .red
+        view.addSubview(redView)
+        
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        // redView's X position and width
+        let horizontalCenter = NSLayoutConstraint(item: redView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1.0, constant: 0)
+        let redViewWidthConstraint = NSLayoutConstraint(item: redView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 200)
+        
+        // redView's Y position and height
+        let redViewBottomSpaceConstraint = NSLayoutConstraint.init(item: redView, attribute: .bottom, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .bottom, multiplier: 1.0, constant: -50)
+        
+        let redViewHeightConstraint = NSLayoutConstraint(item: redView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 100)
+        
+        
+        
+        // Adding redView to the view
+        NSLayoutConstraint.activate([
+            horizontalCenter,
+            redViewWidthConstraint,
+            redViewHeightConstraint,
+            redViewBottomSpaceConstraint
+            
+        ])
+        
     }
 
 
