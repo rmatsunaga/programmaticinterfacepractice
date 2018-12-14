0bfb8a84eb3a7a86b9f4d8009a401f2579b4021a
Rey Matsunaga <rmatsun1@Reys-Mac-mini.local>
2018-12-13 17:06:51 -0800

    refactor using NSLayoutAnchor

--- ProgrammaticAutoLayout Practice/ProgrammaticAutoLayout Practice/ViewController.swift
+++ ProgrammaticAutoLayout Practice/ProgrammaticAutoLayout Practice/ViewController.swift
@@ -27,23 +27,12 @@ class ViewController: UIViewController {
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         
-        // redView's X position and width
-        let horizontalCenter = NSLayoutConstraint(item: redView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1.0, constant: 0)
-        let redViewWidthConstraint = NSLayoutConstraint(item: redView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 200)
-        
-        // redView's Y position and height
-        let redViewBottomSpaceConstraint = NSLayoutConstraint.init(item: redView, attribute: .bottom, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .bottom, multiplier: 1.0, constant: -50)
-        
-        let redViewHeightConstraint = NSLayoutConstraint(item: redView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 100)
-        
-        
-        
         // Adding redView to the view
         NSLayoutConstraint.activate([
-            horizontalCenter,
-            redViewWidthConstraint,
-            redViewHeightConstraint,
-            redViewBottomSpaceConstraint
+            redView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            redView.widthAnchor.constraint(equalToConstant: 200),
+            redView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
+            redView.heightAnchor.constraint(equalToConstant: 100.0)
             
         ])
         
